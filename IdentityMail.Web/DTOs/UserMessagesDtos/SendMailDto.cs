80a1f41f6d14223354b8a8d469354d562e5f0367
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-08-12 16:52:26 +0300

    login_register_pages

---
+++ IdentityMail.Web/DTOs/UserMessagesDtos/SendMailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityMail.Web.DTOs.UserMessagesDtos
+{
+    public class SendMailDto
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
