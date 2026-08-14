94cbb4874a68f85c0c4c8146f7db17a00c788405
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-08-14 19:03:57 +0300

    error_pages

---
+++ IdentityMail.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityMail.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
--- IdentityMail.Web/Program.cs
+++ IdentityMail.Web/Program.cs
@@ -41,6 +41,7 @@ builder.Services.ConfigureApplicationCookie(config =>
     config.LoginPath = "/Auth/Login";
     config.LogoutPath = "/Auth/LogOut";
     config.Cookie.Name = "IdentityMailCookie";
+    config.AccessDeniedPath = "/Error/AccessDenied";
 
 });
 
@@ -55,6 +56,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStatusCodePagesWithReExecute("/Error/Error404", "?code={0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
